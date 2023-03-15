3e220fc29e6611ae94a17f877cebe33d6bc10bed
gxdprog <91539650+gxdprog@users.noreply.github.com>
2023-03-15 12:34:48 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -11,38 +11,76 @@ Base = declarative_base()
 def create_db():
     Base.metadata.create_all(engine)
 
-class Logins(Base):
-    __tablename__ = "Logins"
+# class Logins(Base):
+#     __tablename__ = "Logins"
 
-    username = Column(String, primary_key=True)
-    password = Column(String)
+#     username = Column(String, primary_key=True)
+#     password = Column(String)
 
-    def tuple(self):
-        return (self.username, self.password)
-    def at(self, index: int):
-        return self.tuple()[index]
+#     def tuple(self):
+#         return (self.username, self.password)
+#     def at(self, index: int):
+#         return self.tuple()[index]
 
 class Hotels(Base):
     __tablename__ = "Hotels"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    place = Column(String)
+    name = Column(String(150))
+    region = Column(ForeignKey("Region.name"))
 
     def tuple(self):
-        return (self.id, self.name, self.place)
+        return (self.id, self.name, self.region)
+    def at(self, index: int):
+        return self.tuple()[index]
+    
+class Room_capacity(Base):
+    __tablename__ = "Room_capacity"
+
+    id = Column(String, primary_key=True)
+    hotel = Column(ForeignKey("Hotels.id"))
+    
+    def tuple(self):
+        return (self.id, self.hotel)
     def at(self, index: int):
         return self.tuple()[index]
 
+    
 class Room(Base):
     __tablename__ = "Room"
 
     id = Column(Integer, primary_key=True)
-    number = Column(String)
-    floor = Column(String)
+    hotel = Column(ForeignKey("Hotels.id"))
+    number = Column(String(10))
+    category = Column(String) #стандарт люкс апартамент
+    seats = Column(Integer) # стандарт и люкс(до 2) апартамент(до 4) 
+    status = Column(String) # занят, занят грязный, свободен чистый, свободен грязный
 
-class Reservation(Base):
-    __tablename__ = "Reservation"
+    
+    def tuple(self):
+        return (self.id, self.hotel, self.number, self.category, self.seats, self.status)
+    def at(self, index: int):
+        return self.tuple()[index]
+    
+class Busy(Base):
+    __tablename__ = "Busy"
 
     id = Column(Integer, primary_key=True)
+    number = Column(ForeignKey("Room.id"))
+    start = Column(Date)
+    end = Column(Date)
+    
+    def tuple(self):
+        return (self.id, self.number, self.start, self.end)
+    def at(self, index: int):
+        return self.tuple()[index]
 
+class Region(Base):
+    __tablename__ = "Region"
+
+    name = Column(String, primary_key=True)
+
+    def tuple(self):
+        return (self.name)
+    def at(self, index: int):
+        return self.tuple()[index]
