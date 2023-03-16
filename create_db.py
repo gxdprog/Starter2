aa3febb483b8da15c9a879a772f288aa78a3945b
gxdprog <91539650+gxdprog@users.noreply.github.com>
2023-03-16 13:51:54 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -43,6 +43,63 @@ class Номера(Base):
     статус = Column(String)
     дата_заезда = Column(Date)
     дата_выезда = Column(Date)
+    группа_гостей = Column(ForeignKey("Списки_гостей.группа"))
 
     def tuple(self):
-        return (self.id, self.номер, self.отель, self.категория, self.кол_во_мест, self.статус, self.дата_заезда, self.дата_выезда)
+        return (self.id, self.номер, self.отель, self.категория, self.кол_во_мест, self.статус, 
+                self.дата_заезда, self.дата_выезда, self.группа_гостей)
+
+class Гости(Base):
+    __tablename__ = "Гости"
+
+    телефон = Column(String(17), primary_key=True)
+    фио = Column(String)
+    группа = Column(ForeignKey("Списки_гостей.группа"))
+    
+    def tuple(self):
+        return (self.телефон, self.фио, self.группа)
+
+class Клиенты(Base):
+    __tablename__ = "Клиенты"
+
+    id = Column(Integer, primary_key=True)
+    плательщик = Column(String(150))
+    вид_плательщика = Column(String)
+        
+    def tuple(self):
+        return (self.id, self.плательщик, self.вид_плательщика)
+    
+class Журнал_бронирования(Base):
+    '''(self.id, self.дата, self.заказчик, self.отель, self.номер, self.дата_заезда, self.дата_выезда)'''
+    __tablename__ = "Журнал_бронирования"
+
+    id = Column(Integer, primary_key=True)
+    дата = Column(Date)
+    заказчик = Column(ForeignKey("Клиенты.id"))
+    отель = Column(ForeignKey("Отели.id"))
+    номер = Column(ForeignKey("Номера.id"))
+    дата_заезда = Column(Date)
+    дата_выезда = Column(Date)
+
+    def tuple(self):
+        return (self.id, self.дата, self.заказчик, self.отель, self.номер, self.дата_заезда, self.дата_выезда)
+    
+class Списки_гостей(Base):
+    __tablename__ = "Списки_гостей"
+
+    группа = Column(Integer, primary_key=True)
+
+    def tuple(self):
+        return (self.группа)
+
+
+class Цены(Base):
+    __tablename__ = "Цены"
+
+    id = Column(Integer, primary_key=True)
+    отель = Column(ForeignKey("Отели.id"))
+    категория = Column(String)
+    цена = Column(Integer)
+
+    def tuple(self):
+        return (self.id, self.отель, self.категория, self.цена)
