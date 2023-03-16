0490752b4c7c93f8349e390519b708c19d81d45b
gxdprog <91539650+gxdprog@users.noreply.github.com>
2023-03-16 13:52:44 +0300

    Update rightunderwindow.py

--- rightunderwindow.py
+++ rightunderwindow.py
@@ -25,7 +25,18 @@ class RightUnderWindow(QWidget):
         elif self.baze == Номера:
             self.add_clickable_widget(f'Добавить номер', 2)
             self.add_clickable_widget(f'Добавить бронь номеру', 3)
-
+        elif self.baze == Журнал_бронирования:
+            self.add_clickable_widget(f'Создать бронь', 5)
+            self.add_clickable_widget(f'Оформить заезд', 8)
+            self.add_clickable_widget(f'Оформить выезд', 10)
+        elif self.baze == Гости:
+            self.add_clickable_widget(f'Добавить гостя', 6)
+        elif self.baze == Клиенты:
+            self.add_clickable_widget(f'Добавить клиента', 7)
+        elif self.baze == Списки_гостей:
+            self.add_clickable_widget(f'Создать группу гостей', 9)
+        elif self.baze == Цены:
+            self.add_clickable_widget(f'Добивать/Изменить данные', 11)
 
         cast(pyqtBoundSignal, self.list_widget.itemClicked).connect(
             self.handle_itemClicked)
@@ -40,5 +51,7 @@ class RightUnderWindow(QWidget):
     def handle_itemClicked(self, item: QListWidgetItem):
         if not hasattr(self, "help_window"):
             self.help_window = None
+        
         self.help_window = set_up_window(item.data(1000))
-        self.help_window.sh.show()
+        if item.data(1000) != 9:
+            self.help_window.sh.show()
